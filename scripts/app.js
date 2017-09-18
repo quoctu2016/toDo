1e602583c4192563f5bdd7496bcf0e6616a1bae7
unknown <you@example.com>
2017-09-18 09:40:12 +0700

    changed app.js

--- scripts/app.js
+++ scripts/app.js
@@ -16,7 +16,7 @@ var app = angular.module('app', [])
     $scope.save = function () {
         if ($scope.name && $scope.time) {
             $scope.todos.push(
-                { name: $scope.name, time: $scope.time, stt: 'Complete' }
+                { name: $scope.name, time: $scope.time, stt: 'Uncomplete' }
                 );
         }
     }
